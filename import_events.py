e616fe66bf91252115ad28b4939e45ea0949a136
Linus Lewandowski <linus@lew21.net>
2022-11-30 21:18:04 +0100

    Import: Wipe and re-create all accounts.

--- import_events.py
+++ import_events.py
@@ -8,6 +8,7 @@ from collections import namedtuple
 from pathlib import Path
 from dataclasses import asdict, dataclass
 from decimal import Decimal
+from django.db import transaction
 import csv
 
 from kw.models import *
@@ -30,9 +31,8 @@ class Entry:
 	doc_party_email: str = None
 	doc_party_iban: str = None
 
-def load_journal():
+def load_journal(p):
 	journal = []
-	p = Path('.')
 
 	for fname in p.glob('dziennik.csv'):
 		with open(fname, 'r') as f:
@@ -56,64 +56,76 @@ def load_journal():
 
 	return sorted(journal, key=lambda entry: (entry.date, entry.us, module_order[entry.module], -entry.amount_pln))
 
+def load_accounts(p):
+	accounts = []
 
-journal = load_journal()
+	for fname in p.glob('konta.csv'):
+		with open(fname, 'r') as f:
+			reader = csv.reader(f, delimiter=';')
+			header = next(reader)
+			RawAccount = namedtuple('RawAccount', header)
+			for rawAccount in reader:
+				rawAccount = RawAccount(*rawAccount)
+				accounts.append(rawAccount)
 
-Event.objects.all().delete()
-Document.objects.all().delete()
+	return accounts
 
-for acc in Account.objects.all().order_by('-num_id'):
-	if acc.num_id.startswith('200-') or len(acc.num_id.split('-')) > 2:
+p = Path('../ksiegi')
+
+with transaction.atomic():
+	Event.objects.all().delete()
+	Document.objects.all().delete()
+	Account.objects.all().delete()
+
+	for account in load_accounts(p):
+		acc = Account(num_id = account.id, name = account.name)
+		acc.save()
+
+	accounts = {acc.num_id: acc for acc in Account.objects.all()}
+
+	def account(id):
+		if '/' in id:
+			account(id.rsplit('/', 1)[0])
+
+		num_id = id.replace('-', '').replace('/', '-')
+
 		try:
-			acc.delete()
-		except:
-			pass
+			return accounts[num_id]
+		except KeyError:
+			acc, created = Account.objects.get_or_create(num_id = num_id, defaults = dict(
+				name = id.rsplit('/')[-1],
+			))
+			return acc
 
-accounts = {acc.num_id: acc for acc in Account.objects.all()}
+	journal = load_journal(p)
 
-names = {}
+	docs = groupby(sorted(journal, key=lambda e: e.doc_id), key=lambda e: (e.us, e.doc_id))
 
-def account(id):
-	if '/' in id:
-		account(id.rsplit('/', 1)[0])
+	for (us, doc_id), entries in docs:
+		entries = list(entries)
 
-	num_id = id.replace('-', '').replace('/', '-')
+		d, created = Document.objects.get_or_create(
+			issuer_name = entries[0].doc_issuer,
+			number = doc_id.replace('-', '/'),
+			defaults = dict(
+				type = DocumentType.get('d'),
+				date = entries[0].doc_issue_date or None,
+			),
+		)
 
-	try:
-		return accounts[num_id]
-	except KeyError:
-		acc, created = Account.objects.get_or_create(num_id = num_id, defaults = dict(
-			name = names.get(id, id.rsplit('/')[-1]),
-		))
-		return acc
+	for entry in journal:
+		src_id = entry.them if entry.amount_pln >= 0 else entry.us
+		dst_id = entry.us if entry.amount_pln >= 0 else entry.them
 
-docs = groupby(sorted(journal, key=lambda e: e.doc_id), key=lambda e: (e.us, e.doc_id))
+		doc = Document.objects.get(issuer_name = entry.doc_issuer, number = entry.doc_id.replace('-', '/'))
 
-for (us, doc_id), entries in docs:
-	entries = list(entries)
-
-	d, created = Document.objects.get_or_create(
-		issuer_name = entries[0].doc_issuer,
-		number = doc_id.replace('-', '/'),
-		defaults = dict(
-			type = DocumentType.get('d'),
-			date = entries[0].doc_issue_date or None,
-		),
-	)
-
-for entry in journal:
-	src_id = entry.them if entry.amount_pln >= 0 else entry.us
-	dst_id = entry.us if entry.amount_pln >= 0 else entry.them
-
-	doc = Document.objects.get(issuer_name = entry.doc_issuer, number = entry.doc_id.replace('-', '/'))
-
-	e = Event(
-		id = f"{entry.module}#{entry.id}",
-		date = entry.date,
-		doc = doc,
-		src = account(src_id),
-		dst = account(dst_id),
-		amount = int((entry.amount_pln if entry.amount_pln >= 0 else -entry.amount_pln) * 100),
-		title = entry.title,
-	)
-	e.save()
+		e = Event(
+			id = f"{entry.module}#{entry.id}",
+			date = entry.date,
+			doc = doc,
+			src = account(src_id),
+			dst = account(dst_id),
+			amount = int((entry.amount_pln if entry.amount_pln >= 0 else -entry.amount_pln) * 100),
+			title = entry.title,
+		)
+		e.save()
