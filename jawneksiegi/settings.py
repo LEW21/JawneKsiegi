72c6f87e1e0ecbdc200c05d123de6eba7f19e541
Linus Lewandowski <linus@lew21.net>
2022-12-01 09:38:52 +0100

    Drop Django admin panel.

--- jawneksiegi/settings.py
+++ jawneksiegi/settings.py
@@ -1,11 +1,6 @@
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 INSTALLED_APPS = (
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'kw',
 )
@@ -13,12 +8,7 @@ INSTALLED_APPS = (
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 TEMPLATES = [
@@ -29,8 +19,6 @@ TEMPLATES = [
 			'context_processors': [
 				'django.template.context_processors.debug',
 				'django.template.context_processors.request',
-				'django.contrib.auth.context_processors.auth',
-				'django.contrib.messages.context_processors.messages',
 			],
 		},
 	},
--- jawneksiegi/urls.py
+++ jawneksiegi/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
-from django.contrib import admin
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
 	path('', include('kw.urls')),
 ]
